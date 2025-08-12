4e7696336913e9b480a45aed38d2d1b521c7fe3c
Loukya Sri <loukyasrikudipudi@gmail.com>
2025-08-12 14:41:17 +0530

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const path = require("path");
 const userRouter = require("./routes/userRoutes");
 const linkRouter = require("./routes/linkRoutes");
 const randomDataRouter = require("./routes/randomDataRoutes");
+const rateLimit = require("express-rate-limit");
+
 const app = express();
+
 app.use(express.json());
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "too many request from this IP, plase try again in an hour",
+});
+
+app.use("/api", limiter);
 
 app.use((req, res, next) => {
   // console.log(req.headers);
--- controller/userController.js
+++ controller/userController.js
@@ -45,6 +45,18 @@ exports.signup = async (req, res) => {
 
     const token = signToken(newUser._id);
 
+    const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+
+    if (process.env.NODE_ENV === "produciton") cookieOptions.secure = true;
+
+    res.cookie("jwt", token, cookieOptions);
+    newUser.password = undefined;
+
     res.status(201).json({
       token,
       status: "success",
