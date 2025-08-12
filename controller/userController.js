89d5687e58760d30daed4a79d3ebad32e3d24916
Loukya Sri <loukyasrikudipudi@gmail.com>
2025-08-12 15:04:32 +0530

    forth commit

--- controller/userController.js
+++ controller/userController.js
@@ -150,11 +150,11 @@ const sendEmail = require("../utils/email"); // Utility for sending emails
 const crypto = require("crypto"); // For generating secure random tokens
 
 // Helper function to sign a JWT token with the user's ID
-const signToken = (id) => {
-  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
+// const signToken = (id) => {
+//   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRES_IN,
+//   });
+// };
 
 // ==============================
 // USER CRUD CONTROLLERS
